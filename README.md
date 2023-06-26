51a87610c6d467f7be71af427cb8af17f0546be0
ericzhang-cn <ericzhang.buaa@gmail.com>
2023-06-26 18:34:14 +0800

    docs: readme

--- README.md
+++ README.md
@@ -15,22 +15,10 @@
 - 🔗**LangChain友好**：方便集成LangChain
 - 🧩**模块化**：项目采用模块化组织，模块之间通过抽象协议依赖，同类模块实现协议即可即插即用
 - 💻**可扩展**：可以扩展AilingBot的使用场景和能力。例如接入到新的IM，新的AI模型，或者定制自己的对话策略
-- 🚀**高性能**：AilingBot采用基于协程的异步模式，提高系统的高并发性能。同时可以通过多进程进一步提升系统的高并发处理能力
+- 🔥**高性能**：AilingBot采用基于协程的异步模式，提高系统的高并发性能。同时可以通过多进程进一步提升系统的高并发处理能力
 - 🔌**通过API集成**：AilingBot提供一组清晰的API接口，方便与其他系统及流程集成协同
 
-# IM支持列表
-
-## 已支持
-
-- 企业微信
-- 飞书
-
-## 即将支持
-
-- 钉钉
-- Slack
-
-# 快速使用指南
+# 🚀快速开始
 
 ## 5分钟启动一个AI聊天机器人
 
@@ -151,6 +139,98 @@ Webhook的URL为：`http(s)://你的公网IP:8080/webhook/wechatwork/event/`
 <p align="center">
     <img src="https://raw.githubusercontent.com/ericzhang-cn/ailingbot/main/img/wechatwork-screenshot.png" alt="企业微信机器人" width="300"/>
 </p>
+
+# 📖使用指南
+
+## 主要流程
+
+AilingBot的主要处理流程如下图：
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/ericzhang-cn/ailingbot/main/img/flow.png" alt="主要流程" width="500"/>
+</p>
+
+1. 首先用户将消息发送给IM的机器人
+2. 如果配置了webhook，即时通讯工具会将发送给机器人的请求转发到webhook服务地址
+3. Webhook服务将IM原始消息经过处理，转为AilingBot内部的消息格式，发送给ChatBot
+4. ChatBot会根据所配置的会话策略（Chat Policy），处理请求并形成响应消息。这个过程中，ChatBot
+   可能会进行请求大语言模型、访问向量数据库、调用外部API等操作以完成请求处理
+5. ChatBot将响应信息发送给IM Agent，IM Agent负责将AilingBot内部响应信息格式转换成
+   特定IM的格式，并调用IM开放能力API发送响应消息
+6. IM机器人将消息显示给用户，完成整个处理过程
+
+## 主要概念
+
+- **IM机器人**：多数即时通讯工具内置的能力，允许管理员创建一个机器人，并通过程序处理用户的消息
+- **Channel**：Channel表示不同终端，可以是一个IM，也可能是一个自定义终端（如Web）
+- **Webhook**：一个http(s)服务，用于接收IM机器人转发的用户消息，不同Channel对于webhook有自己的规范，因此需要有自己的webhook实现
+- **IM Agent**：用于调用IM开放能力API，不同的IM开放能力API不同，因此每个Channel需要有对应Agent实现
+- **ChatBot**：用于接收和响应用户消息的核心组件
+- **会话策略**：具体定义如何响应用户，被ChatBot调用。一个会话策略具体定义了机器人的能力，如闲聊、进行知识问答等
+- **LLM**：大语言模型，如何OpenAI的ChatGPT，开放的ChatGLM等均属于不同的大语言模型，大语言模型是实现AI能力的关键组件
+
+## 配置
+
+### 配置方式
+
+AilingBot的配置可以通过两种方式：
+
+- **通过配置文件**：AilingBot读取当前目录的`settings.toml`作为配置文件，其文件格式为[TOML](https://toml.io/en/)
+  具体配置项见下文
+- **通过环境变量**：AilingBot也会读取环境变量中配置项，具体环境变量列表见下文
+
+> 💡配置文件和环境变量可以混合使用，当一个配置项同时存在于两者时，优先使用环境变量
+
+### 配置项
+
+#### 通用
+
+| 配置项       | 说明                                                                  | TOML                 | 环境变量                             |
+|-----------|---------------------------------------------------------------------|----------------------|----------------------------------|
+| 语言        | 语言码（参考：http://www.lingoes.net/en/translator/langcode.htm）           | lang                 | AILINGBOT__LANG                  |
+| 时区        | 时区码（参考：https://en.wikipedia.org/wiki/List_of_tz_database_time_zones | tz                   | AILINGBOT__TZ                    |
+| 会话策略名称    | 预置会话策略名称或完整会话策略class路径                                              | policy.name          | AILINGBOT__POLICY__NAME          |
+| Channel名称 | 预置Channel名称                                                         | channel.name         | AILINGBOT__CHANNEL__NAME         |
+| Webhook路径 | 非预置Channel webhook的完整class路径                                        | channel.webhook_name | AILINGBOT__CHANNEL__WEBHOOK_NAME |
+| Agent路径   | 非预置Channel agent的完整class路径                                          | channel.agent_name   | AILINGBOT__CHANNEL__AGENT_NAME   |
+| Uvicorn配置 | 所有uvicorn配置（参考：https://www.uvicorn.org/settings/)，这部分配置会透传给uvicorn  | uvicorn.*            | AILINGBOT__CHANNEL__UVICORN__*   |
+
+#### 内置会话策略配置
+
+##### lc_conversation
+
+lc_conversation使用LangChain的Conversation作为会话策略，其效果为直接和LLM对话，且带有对话历史上下文，因此可以进行多轮会话。
+
+| 配置项 | 说明 | TOML | 环境变量 |
+|-----|----|------|------|
+
+##### lc_document_qa
+
+lc_document_qa使用LangChain的[Stuff](https://python.langchain.com/docs/modules/chains/document/stuff)作为对话策略。
+用户可上传一个文档，然后针对文档内容进行提问。
+
+| 配置项 | 说明 | TOML | 环境变量 |
+|-----|----|------|------|
+
+#### 内置Channel配置
+
+##### 企业微信
+
+| 配置项 | 说明 | TOML | 环境变量 |
+|-----|----|------|------|
+
+##### 飞书
+
+| 配置项 | 说明 | TOML | 环境变量 |
+|-----|----|------|------|
+
+## 命令行工具
+
+TBD
+
+# 💻开发指南
+
+TBD
 
 # 发展计划
 
--- ailingbot/chat/policies/langchain.py
+++ ailingbot/chat/policies/langchain.py
@@ -32,9 +32,9 @@ class LCConversationChatPolicy(ChatPolicy):
     """Having a direct conversation with a large language model."""
 
     def __init__(
-        self,
-        *,
-        debug: bool = False,
+            self,
+            *,
+            debug: bool = False,
     ):
         super(LCConversationChatPolicy, self).__init__(
             debug=debug,
@@ -61,11 +61,11 @@ class LCConversationChatPolicy(ChatPolicy):
         return self.memories[conversation_id]
 
     async def respond(
-        self, *, conversation_id: str, message: RequestMessage
+            self, *, conversation_id: str, message: RequestMessage
     ) -> ResponseMessage:
         if not isinstance(message, TextRequestMessage):
             response = FallbackResponseMessage()
-            response.reason = 'LCConversationChatPolicy can only handle messages of type TextRequestMessage.'
+            response.reason = '不支持的消息类型'
         else:
             if conversation_id not in self.chain:
                 self.chain.memory = await self._load_memory(
@@ -86,9 +86,9 @@ class LCDocumentQAPolicy(ChatPolicy):
     """Question-Answering based on documents."""
 
     def __init__(
-        self,
-        *,
-        debug: bool = False,
+            self,
+            *,
+            debug: bool = False,
     ):
         super(LCDocumentQAPolicy, self).__init__(
             debug=debug,
@@ -97,17 +97,16 @@ class LCDocumentQAPolicy(ChatPolicy):
         llm_config = copy.deepcopy(settings.policy.llm)
         self.llm = load_llm_from_config(llm_config)
         self.chains: dict[str, Chain] = {}
-        self.trunk_size = settings.policy.trunk_size or 1000
-        self.trunk_overlap = settings.policy.trunk_overlap or 0
+        self.chunk_size = settings.policy.chunk_size or 1000
+        self.chunk_overlap = settings.policy.chunk_overlap or 0
 
-    @staticmethod
     def _build_documents_index(
-        *, content: bytes, file_type: str
+            self, *, content: bytes, file_type: str
     ) -> VectorStoreRetriever:
         """Load document and build index."""
         if file_type.lower() != 'pdf':
             raise ailingbot.shared.errors.ChatPolicyError(
-                reason='Currently only PDF file are supported.',
+                reason='目前只支持PDF文档',
             )
 
         with tempfile.NamedTemporaryFile(delete=True) as tf:
@@ -115,7 +114,7 @@ class LCDocumentQAPolicy(ChatPolicy):
             loader = PyPDFLoader(file_path=tf.name)
             documents = loader.load()
 
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        text_splitter = CharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
         texts = text_splitter.split_documents(documents)
 
         embeddings = OpenAIEmbeddings(
@@ -126,12 +125,12 @@ class LCDocumentQAPolicy(ChatPolicy):
         return docsearch.as_retriever()
 
     async def respond(
-        self, *, conversation_id: str, message: RequestMessage
+            self, *, conversation_id: str, message: RequestMessage
     ) -> ResponseMessage:
         if isinstance(message, TextRequestMessage):
             if conversation_id not in self.chains:
                 response = FallbackResponseMessage()
-                response.reason = 'Please upload the document first.'
+                response.reason = '请先上传文档'
             else:
                 response = TextResponseMessage()
                 response.text = await self.chains[conversation_id].arun(
@@ -147,10 +146,10 @@ class LCDocumentQAPolicy(ChatPolicy):
                 return_source_documents=False,
             )
             response = TextResponseMessage()
-            response.text = f'I am ready! Please ask questions regarding the content of {message.file_name}.'
+            response.text = f'我已完成学习，现在可以针对 {message.file_name} 进行提问了'
         else:
             response = FallbackResponseMessage()
-            response.reason = 'Unsupported Request message type.'
+            response.reason = '不支持的消息类型'
 
         response.uuid = str(uuid.uuid4())
         response.ack_uuid = message.uuid
